758da0f0be774087bd95e84fb5f51b5fa2acdc70
Nayana333 <nrnayana1@gmail.com>
2025-05-06 16:05:13 +0530

    design changed

--- apps/nx_crud/src/components/todo-list.tsx
+++ apps/nx_crud/src/components/todo-list.tsx
@@ -1,6 +1,5 @@
 import { Card, Typography, Input, Button, List, Tooltip, Empty, Radio, Badge, Divider } from 'antd'
-import React from 'react'
-import { UndoOutlined, DeleteOutlined } from "@ant-design/icons"
+import { UndoOutlined, DeleteOutlined ,SaveOutlined,EditOutlined } from "@ant-design/icons"
 import '../libs/todo_styles.scss';
 
 const TodoList = () => {
@@ -12,21 +11,34 @@ const TodoList = () => {
       </div>
 
       <List
-        renderItem={() => (
-          <List.Item className="todo-item group">
-            <div className="flex-1 flex items-center">
-              <Tooltip>
-                <Button type="text" icon={<UndoOutlined />} style={{ color: "#bfbfbf" }} />
-              </Tooltip>
-              <Typography.Text className="todo-text">Sample Todo</Typography.Text>
-            </div>
-            <Tooltip title="Delete">
-              <Button type="text" danger icon={<DeleteOutlined />} className="opacity-transition" />
-            </Tooltip>
-          </List.Item>
-        )}
-        locale={{ emptyText: <Empty description="No todos found" image={Empty.PRESENTED_IMAGE_SIMPLE} /> }}
-      />
+  renderItem={() => (
+    <List.Item className="todo-item group">
+      <div className="flex-1 flex items-center gap-2">
+        <Tooltip title="Complete / Undo">
+          <Button type="text" icon={<UndoOutlined />} style={{ color: "#bfbfbf" }} />
+        </Tooltip>
+
+        <Typography.Text className="todo-text">Sample Todo</Typography.Text>
+
+        <Input defaultValue="Sample Todo" className="todo-edit-input" style={{ display: 'none', width: 200 }} />
+
+        <Tooltip title="Edit">
+          <Button type="text" icon={<EditOutlined />} />
+        </Tooltip>
+
+        <Tooltip title="Save">
+          <Button type="text" icon={<SaveOutlined />} />
+        </Tooltip>
+
+        <Tooltip title="Delete">
+          <Button type="text" danger icon={<DeleteOutlined />} className="opacity-transition" />
+        </Tooltip>
+      </div>
+    </List.Item>
+  )}
+  locale={{ emptyText: <Empty description="No todos found" image={Empty.PRESENTED_IMAGE_SIMPLE} /> }}
+/>
+
 
       <Divider style={{ borderColor: "#303030" }} />
 
@@ -34,12 +46,6 @@ const TodoList = () => {
         <Badge showZero color="#1890ff">
           <Typography.Text style={{ color: "#bfbfbf" }}>Items left</Typography.Text>
         </Badge>
-
-        <Radio.Group optionType="button" buttonStyle="solid">
-          <Radio.Button value="all">All</Radio.Button>
-          <Radio.Button value="active">Active</Radio.Button>
-          <Radio.Button value="completed">Completed</Radio.Button>
-        </Radio.Group>
 
         <Button danger type="text">Clear Completed</Button>
       </div>
