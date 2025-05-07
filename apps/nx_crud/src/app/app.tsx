7d1001aaa2dc0b68c9082e716623c98118a0c708
Nayana333 <nrnayana1@gmail.com>
2025-05-07 11:16:59 +0530

    redux completed

--- apps/nx_crud/src/app/app.tsx
+++ apps/nx_crud/src/app/app.tsx
@@ -1,5 +1,4 @@
 
-import React from 'react';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { BrowserRouter } from 'react-router-dom';
